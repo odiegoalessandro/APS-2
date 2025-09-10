89725a54aaa3436596d125e5a36d58319046fc7a
Diego A.C. Martins <diegoalessandrodacruzmartins@gmail.com>
2025-09-10 10:16:46 -0300

    feat: criando as funcoes auxiliares

---
+++ get_key.py
@@ -0,0 +1,7 @@
+def gen_key(p: int, q: int) -> int:
+    n = p * q
+    z = (p - 1) * (q - 1)
+
+    
+
+    return 0
---
+++ modinv.py
@@ -0,0 +1,2 @@
+def modinv(e: int, phi_n: int) -> int:
+    return pow(e, -1, phi_n)
